254c487aec3faf51dcfe3af8358895bf3686062d
prince-arun <prince.skillsafari@gmail.com>
2023-09-09 18:33:22 +0530

    source and tailwind setup completed

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-5xl font-bold">Rhythmo</h1>
+    </div>
+  );
+}
+
+export default App;
