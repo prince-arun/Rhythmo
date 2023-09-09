c57ba2be9fe5bdc23a4911d4c20be260dc26ff5d
prince-arun <prince.skillsafari@gmail.com>
2023-09-09 22:43:40 +0530

    model-Schema for registration

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//creating schema for registration-api
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    playlists: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+//exporting model
+module.exports = mongoose.model("User", userSchema);
