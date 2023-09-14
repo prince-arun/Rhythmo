18df1bfd60a0bf87e940a84cd4da9ce23f3611a0
prince-arun <prince.skillsafari@gmail.com>
2023-09-14 10:42:58 +0530

    Home page header finished

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function DefaultLayout({ children }) {
   const { user } = useSelector((state) => state.user);
@@ -17,10 +18,15 @@ function DefaultLayout({ children }) {
           <b className="text-primary">RHYTHMO</b>{" "}
         </h1>
         <div className="flex items-center gap-2">
+          <Link to="/admin">
+            <h1 className="text-xl">Admin Panel</h1>
+          </Link>
+
           <h1 className="text-xl">{user?.name.toUpperCase()}</h1>
           <i
             className="ri-logout-circle-r-line text-xl cursor-pointer"
             onClick={() => {
+              toast.error("You are Logged Out");
               localStorage.removeItem("token");
               window.location.reload();
             }}
