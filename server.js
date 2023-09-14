a283fdd7c7daf05d748ddaf9af85221e783fc117
prince-arun <prince.skillsafari@gmail.com>
2023-09-14 14:13:29 +0530

    Hosting Scripts added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
@@ -12,5 +13,11 @@ const adminRoute = require("./routes/adminRoute");
 app.use("/api/users", userRoute);
 app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`server started at port  ${PORT}`));
