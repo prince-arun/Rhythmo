e962ebd81109850168afe77ee0a7846f2d47b958
prince-arun <prince.skillsafari@gmail.com>
2023-09-14 10:49:45 +0530

    Alternate route for songlist

---
+++ client/src/components/api.js
@@ -0,0 +1,24 @@
+// api.js
+import axios from "axios";
+export const fetchAllSongs = async () => {
+  try {
+    const response = await axios.post(
+      "/api/songs/get-all-songs",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch songs");
+    }
+    console.log(response);
+
+    return response.data.data; // Assuming the API response contains the songs data
+  } catch (error) {
+    throw error;
+  }
+};
